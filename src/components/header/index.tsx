97429fbb981b375964c00ee15131f4e0a683d711
chris <chris@modugen.de>
2022-08-11 11:03:56 +0200

    Add link to homepage in header bar

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -4,6 +4,7 @@ import Box from '@mui/material/Box'
 import Toolbar from '@mui/material/Toolbar'
 import { useTheme } from '@mui/material'
 import logo from '../../assets/icons/modugen-log.svg'
+import { Link } from 'react-router-dom'
 
 function Header() {
   const theme = useTheme()
@@ -12,7 +13,9 @@ function Header() {
     <Box marginBottom={theme.spacing(10)} sx={{ display: 'flex' }}>
       <AppBar sx={{ backgroundColor: theme.palette.grey[100] }} component='nav'>
         <Toolbar>
-          <img style={{ width: theme.spacing(5) }} src={logo} />
+          <Link to="/">
+            <img style={{ width: theme.spacing(5) }} src={logo} />
+          </Link>
           <Box sx={{ display: { xs: 'none', sm: 'block' } }}></Box>
         </Toolbar>
       </AppBar>

a65138a2f452a244e3f34417c430cc7f93a5984e
Sandro Braun <supermario94123@gmail.com>
2022-08-11 09:16:28 +0200

    Format code

--- src/calc.tsx
+++ src/calc.tsx
@@ -3,31 +3,31 @@ enum RelativeWohnlage {
     AmEck = "AmEck"
     Innenliegend = "Innenliegend"
     Freistehend = "Freistehend"
-  }
-  
-  enum Lage {
+}
+
+enum Lage {
     DG = "DG"
-    EG = "EG"
-    Zwischengeschoss = "Zwischengeschoss"
-    DG_EG = "DG+EG"
-  }
-  
-  enum Bausubstanz {
+EG = "EG"
+Zwischengeschoss = "Zwischengeschoss"
+DG_EG = "DG+EG"
+}
+
+enum Bausubstanz {
     Altbau = "Altbau"
-    AltbauSaniert = "AltbauSaniert"
-    Neubau= "Neubau"
-  }
-  
-  enum Heizungsart {
+AltbauSaniert = "AltbauSaniert"
+Neubau = "Neubau"
+}
+
+enum Heizungsart {
     Gas = "Gas"
-    Strom = "Strom"
-    Pellets = "Pellets"
-    Fernwaerme = "Fernwaerme"
-  
-  }
-  
-  
-  interface ConfiguratorParameters {
+Strom = "Strom"
+Pellets = "Pellets"
+Fernwaerme = "Fernwaerme"
+
+}
+
+
+interface ConfiguratorParameters {
     wohnflaeche: number
     relativeWohnlage: RelativeWohnlage
     lage: Lage
@@ -36,33 +36,33 @@ enum RelativeWohnlage {
     heizungsart: Heizungsart
     fensterflaecheRelativ: number
     fensterflaecheAbsolut: number // todo maybe just use one of these, and do the calculation in the input fields
-  }
-  
-  
-  
-  function calc_external_wall_area(params: ConfiguratorParameters): number {
+}
+
+
+
+function calc_external_wall_area(params: ConfiguratorParameters): number {
     const singleOuterSurfaceArea = Math.sqrt(params.wohnflaeche) * params.deckenhoehe
     if (params.relativeWohnlage == RelativeWohnlage.AmEck) {
-      return singleOuterSurfaceArea * 2
+        return singleOuterSurfaceArea * 2
     }
     else if (params.relativeWohnlage == RelativeWohnlage.Freistehend) {
-      return singleOuterSurfaceArea * 4
+        return singleOuterSurfaceArea * 4
     }
     else if (params.relativeWohnlage == RelativeWohnlage.Innenliegend) {
-      return singleOuterSurfaceArea * 1
-    } 
-    else {
-      // raise error here?
-      return 0
+        return singleOuterSurfaceArea * 1
     }
-  }
-      
+    else {
+        // raise error here?
+        return 0
+    }
+}
+
 function calc_ceiling_and_floor_area(params: ConfiguratorParameters): number {
     switch (params.lage) {
         case Lage.DG:
-          return params.wohnflaeche
+            return params.wohnflaeche
         case Lage.EG:
-          return params.wohnflaeche
+            return params.wohnflaeche
         case Lage.Zwischengeschoss:
             return 0
         case Lage.DG_EG:
@@ -70,7 +70,6 @@ function calc_ceiling_and_floor_area(params: ConfiguratorParameters): number {
         default:
             return 0
     }
-  }
-  
-  
-  
+}
+
+

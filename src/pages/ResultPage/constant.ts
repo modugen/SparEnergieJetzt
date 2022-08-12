4e84933ad2270f74ee53dea08b596ee21dfc8f03
chris <chris@modugen.de>
2022-08-12 18:28:42 +0200

    Update links and text

--- src/pages/ResultPage/constant.ts
+++ src/pages/ResultPage/constant.ts
@@ -33,12 +33,13 @@ export interface ResultProposal {
 export const results: ResultProposal[] = [
   {
     title: 'Heizkörper Reflektionsfolie',
-    description: 'Die mit Aluminium beschichteten Folien werden hinter den Heizkörpern so an der Wand angebracht, dass die beschichtete Seite zum Heizkörper zeigt. Durch die Reflektion der Wärmestrahlung wird verhindert, dass die vom Heizelement abgegebene Wärme die  Außenwand erwärmt statt den Raum zu heizen.',
+    description:
+      'Die mit Aluminium beschichteten Folien werden hinter den Heizkörpern so an der Wand angebracht, dass die beschichtete Seite zum Heizkörper zeigt. Durch die Reflektion der Wärmestrahlung wird verhindert, dass die vom Heizelement abgegebene Wärme die  Außenwand erwärmt statt den Raum zu heizen.',
     image: heizkorper,
     winner: true,
     links: [
-      'https://amzn.to/3OWoMNU', 
-      'https://www.google.com/search?q=Heizk%C3%B6rper+Reflektionsfolie&sxsrf=ALiCzsbIvtBTtK4DJZfRGKMc0QYD5HRWMQ:1660313326812&source=lnms&tbm=shop&sa=X&ved=2ahUKEwj7ob2QvcH5AhWOi_0HHU7PBdoQ_AUoAXoECAIQAw&biw=1440&bih=732&dpr=2'
+      'https://amzn.to/3OWoMNU',
+      'https://www.google.com/search?q=Heizk%C3%B6rper+Reflektionsfolie&sxsrf=ALiCzsbIvtBTtK4DJZfRGKMc0QYD5HRWMQ:1660313326812&source=lnms&tbm=shop&sa=X&ved=2ahUKEwj7ob2QvcH5AhWOi_0HHU7PBdoQ_AUoAXoECAIQAw&biw=1440&bih=732&dpr=2',
     ],
     type: 'reflection',
     calculation: calcSavingsReflexionsfolie,
@@ -46,11 +47,12 @@ export const results: ResultProposal[] = [
   },
   {
     title: 'Heizkörperbürste',
-    description: 'Staub, Schmutz und andere Verunreinigungen verringern die Wärmeabgabe des Heizkörpers. Am besten reinigst du deinen Heizkörper mit Hilfe einer Heizkörperbürste und steigerst so die Effizienz deiner Heizung. Besonders bei geschlossenen Plattenheizkörpern ist diese Maßnahme relevant!',
+    description:
+      'Staub, Schmutz und andere Verunreinigungen verringern die Wärmeabgabe des Heizkörpers. Am besten reinigst du deinen Heizkörper mit Hilfe einer Heizkörperbürste und steigerst so die Effizienz deiner Heizung. Besonders bei geschlossenen Plattenheizkörpern ist diese Maßnahme relevant!',
     image: heizungsburste,
     links: [
-      'https://amzn.to/3Sme3PK', 
-      'https://www.google.com/search?q=Heizk%C3%B6rperb%C3%BCrste+google+shopping&sxsrf=ALiCzsZiIfTMam1JeJf0FCrXAIK-S9IkHA:1660289698749&source=lnms&tbm=shop&sa=X&ved=2ahUKEwiw_t6N5cD5AhX1i_0HHU-CCnkQ_AUoAXoECAEQAw&biw=1440&bih=789&dpr=2'
+      'https://amzn.to/3Sme3PK',
+      'https://www.google.com/search?q=Heizk%C3%B6rperb%C3%BCrste+google+shopping&sxsrf=ALiCzsZiIfTMam1JeJf0FCrXAIK-S9IkHA:1660289698749&source=lnms&tbm=shop&sa=X&ved=2ahUKEwiw_t6N5cD5AhX1i_0HHU-CCnkQ_AUoAXoECAEQAw&biw=1440&bih=789&dpr=2',
     ],
     type: 'heizkoerperbuerste',
     calculation: calcSavingsHeizkoerperbuerste,
@@ -58,11 +60,12 @@ export const results: ResultProposal[] = [
   },
   {
     title: 'Thermoheld',
-    description: 'Das Thermoheld Heizglas ist eine elektrische Heizung, die anstatt die Raumluft zu beheizen Wärmestrahlung abgibt. So kannst du die Raumtemperatur ohne Komfortverlust senken. Durch den Betrieb mit Strom bist du unabhängig von fossilen Brennstoffen, und kannst sogar deine Photovoltaik-Anlage mit anschließen.',
+    description:
+      'Das Thermoheld Heizglas ist eine elektrische Heizung, die anstatt die Raumluft zu beheizen Wärmestrahlung abgibt. So kannst du die Raumtemperatur ohne Komfortverlust senken. Durch den Betrieb mit Strom bist du unabhängig von fossilen Brennstoffen, und kannst sogar deine Photovoltaik-Anlage mit anschließen.',
     image: thermoheld,
     links: [
       'https://www.thermoheld.global/thermoheld-glas/',
-      'https://www.hornbach.de/aktuelles/ethermoheld-so-geht-waerme-heute/'
+      'https://www.hornbach.de/aktuelles/ethermoheld-so-geht-waerme-heute/',
     ],
     type: 'thermoheld',
     calculation: calcSavingsThermoheld,
@@ -75,8 +78,8 @@ export const results: ResultProposal[] = [
       'Warmwasser stellt einen großen Energiefresser dar. Mit einem wassersparenden Duschkopf lassen sich sogar im Sommer große Einsparungen erzielen. Je nach Modell werden bis zu 30% weniger Wasser durch den Duschkopf gelassen, bei gleichem Duschgefühl.',
     image: wassersparender,
     links: [
-      'https://amzn.to/3cZlLiO', 
-      'https://www.google.com/search?q=Wassersparender+Duschkopf&sxsrf=ALiCzsYAQAYHFPXrtebRQSB5LzY11-hH0g:1660289732868&source=lnms&tbm=shop&sa=X&ved=2ahUKEwj-rYGe5cD5AhUfgv0HHYlNDWgQ_AUoAXoECAEQAw&biw=1440&bih=789&dpr=2'
+      'https://amzn.to/3cZlLiO',
+      'https://www.google.com/search?q=Wassersparender+Duschkopf&sxsrf=ALiCzsYAQAYHFPXrtebRQSB5LzY11-hH0g:1660289732868&source=lnms&tbm=shop&sa=X&ved=2ahUKEwj-rYGe5cD5AhUfgv0HHYlNDWgQ_AUoAXoECAEQAw&biw=1440&bih=789&dpr=2',
     ],
     type: 'duschkopf',
     calculation: calcSavingsDuschkopf,
@@ -88,8 +91,8 @@ export const results: ResultProposal[] = [
       'Dieser Timer hilft dir deine Energiekosten beim Duschen oder anderen Tätigkeiten zu senken. Klingt vielleicht lustig, ist aber effektiv. Mit unserem Verhalten haben wir mit den größten Einfluss auf unseren Energiebedarf. Ein Timer kann helfen, sich die Duschzeiten präsent zu machen - und zu reduzieren.',
     image: timer,
     links: [
-      'https://amzn.to/3Qb7ayX', 
-      'https://www.google.com/search?q=Duschtimer&sxsrf=ALiCzsbgXeIXCgoncksFQK-qY-sZrXZDew:1660313228245&source=lnms&tbm=shop&sa=X&ved=2ahUKEwiBv73hvMH5AhU3xQIHHVtUD5IQ_AUoAXoECAIQAw&biw=1440&bih=732&dpr=2'
+      'https://amzn.to/3Qb7ayX',
+      'https://www.google.com/search?q=Duschtimer&sxsrf=ALiCzsbgXeIXCgoncksFQK-qY-sZrXZDew:1660313228245&source=lnms&tbm=shop&sa=X&ved=2ahUKEwiBv73hvMH5AhU3xQIHHVtUD5IQ_AUoAXoECAIQAw&biw=1440&bih=732&dpr=2',
     ],
     type: 'timer',
     calculation: calcSavingsTimer,
@@ -102,7 +105,7 @@ export const results: ResultProposal[] = [
     image: dichtbander,
     links: [
       'https://amzn.to/3vulM4w',
-      'https://www.google.com/search?q=Dichtb%C3%A4nder+f%C3%BCr+Kastenfenster&sxsrf=ALiCzsZwwxrOdEzHLt7zXCYKbL61kGe6cA:1660313349762&source=lnms&tbm=shop&sa=X&ved=2ahUKEwjRi7abvcH5AhXu_rsIHXIgAS0Q_AUoAXoECAEQAw&biw=1440&bih=732&dpr=2'
+      'https://www.google.com/search?q=Dichtb%C3%A4nder+f%C3%BCr+Kastenfenster&sxsrf=ALiCzsZwwxrOdEzHLt7zXCYKbL61kGe6cA:1660313349762&source=lnms&tbm=shop&sa=X&ved=2ahUKEwjRi7abvcH5AhXu_rsIHXIgAS0Q_AUoAXoECAEQAw&biw=1440&bih=732&dpr=2',
     ],
     type: 'dichtbaender',
     calculation: calcSavingsDichtbaenderKastenfenster,
@@ -114,8 +117,8 @@ export const results: ResultProposal[] = [
       'Thermovorhänge: Gerade bei Altbauten stellen schlecht gedämmte Fenster und fehlende Rolladen eine große Schwachstelle dar und führen zu Wärmeverlust. Die Wärmeabgabe durch das Fenster lässt sich durch raumseitige, schwere Vorhänge vermindern.',
     image: thermovorhange,
     links: [
-      'https://amzn.to/3SmnM8C', 
-      'https://www.google.com/search?q=Thermovorh%C3%A4nge&source=lmns&tbm=shop&bih=789&biw=1440&hl=de&sa=X&ved=2ahUKEwij1sXP5cD5AhVOYxoKHbZ4CJkQ_AUoAXoECAEQAQ'
+      'https://amzn.to/3SmnM8C',
+      'https://www.google.com/search?q=Thermovorh%C3%A4nge&source=lmns&tbm=shop&bih=789&biw=1440&hl=de&sa=X&ved=2ahUKEwij1sXP5cD5AhVOYxoKHbZ4CJkQ_AUoAXoECAEQAQ',
     ],
     type: 'thermovorhaenge',
     calculation: calcSavingsThermovorhaenge,
@@ -127,8 +130,8 @@ export const results: ResultProposal[] = [
       'Digitale Thermostate sorgen dafür, dass deine Räume gleichmäßig und somit energiesparend beheizt werden und nur zu den Zeitpunkten beheizt werden, an denen du es wirklich brauchst. Ganz nebenbei machst du dein Haus zum Smart Home. Einfach mal die Heizung von Unterwegs ausmachen, wenn man es vergessen hat? Kein Problem!',
     image: thermostat,
     links: [
-      'https://amzn.to/3oPxFOU', 
-      'https://www.google.com/search?q=heizk%C3%B6rperthermostat&sxsrf=ALiCzsaCD-ZJf3tVzwjP5Y_v-_ZoYMdrsA:1660289924112&source=lnms&tbm=shop&sa=X&ved=2ahUKEwjflJr55cD5AhXLX8AKHcFYCLIQ_AUoAXoECAEQAw&biw=1440&bih=789&dpr=2'
+      'https://amzn.to/3oPxFOU',
+      'https://www.google.com/search?q=heizk%C3%B6rperthermostat&sxsrf=ALiCzsaCD-ZJf3tVzwjP5Y_v-_ZoYMdrsA:1660289924112&source=lnms&tbm=shop&sa=X&ved=2ahUKEwjflJr55cD5AhXLX8AKHcFYCLIQ_AUoAXoECAEQAw&biw=1440&bih=789&dpr=2',
     ],
     type: 'hydraulischeThermostate',
     calculation: calcSavingsThermostate,

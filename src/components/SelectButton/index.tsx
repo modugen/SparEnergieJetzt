f3ba0bac33050622800757378c4a087e838a40b6
chris <chris@modugen.de>
2022-08-12 15:38:13 +0200

    Add breakpoints for mobile view

--- src/components/SelectButton/index.tsx
+++ src/components/SelectButton/index.tsx
@@ -28,48 +28,52 @@ export function SelectButton({
   const theme = useTheme()
 
   return (
-    <Stack direction='column' spacing={1}
+    <Stack
+      direction='column'
+      spacing={1}
       sx={{
         backgroundColor: selected ? green[100] : undefined,
         display: 'inline-block',
         padding: {
           xs: theme.spacing(3),
-          md: theme.spacing(4)
+          md: theme.spacing(4),
         },
         boxShadow: 3,
         cursor: onClick ? 'pointer' : undefined,
         width: {
-          xs: 50, 
-          md: 100
+          xs: 50,
+          md: 100,
         },
       }}
       onClick={onClick}
     >
       <Center>
-        <Box sx={{ 
-          height: {
-            xs: 40, 
-            md: 80
-          }, 
-          width: {
-            xs: 60, 
-            md: 100
-          }, 
-          display: 'flex', 
-          alignItems: 'center', 
-          justifyContent: 'center' 
-        }}>
+        <Box
+          sx={{
+            height: {
+              xs: 40,
+              md: 80,
+            },
+            width: {
+              xs: 60,
+              md: 100,
+            },
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
           <img
             src={img}
             style={{
               maxHeight: '100%',
-              maxWidth: '100%'
+              maxWidth: '100%',
             }}
           />
         </Box>
       </Center>
       <Box minHeight='3em' display='flex' alignItems='center' justifyContent='center' marginTop={1}>
-        <Typography textAlign='center' lineHeight='1.5em' fontSize={{xs: '0.8em', md: '1em'}}>
+        <Typography textAlign='center' lineHeight='1.5em' fontSize={{ xs: '0.8em', md: '1em' }}>
           {text}
         </Typography>
       </Box>

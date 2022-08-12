7d9d919d61f235742d1ec80d412b4bb6b0f19bee
chris <chris@modugen.de>
2022-08-12 18:37:44 +0200

    Improve layout of result buttons

--- src/pages/ResultPage/component/resultCard/BuyButton.tsx
+++ src/pages/ResultPage/component/resultCard/BuyButton.tsx
@@ -4,15 +4,15 @@ import { BuyButton } from '../styled'
 
 const BuyButtons = ({ links }: { links: string[] }) => {
   return (
-    <Grid2 container display='flex' justifyContent='flex-end' spacing={1}>
-      {links.map(link => (
-        <Grid2 key={link} >
-          <BuyButton variant='body1' href={link}>
-            kaufen
-          </BuyButton>
-        </Grid2>
-      ))}
-    </Grid2>
+      <Grid2 container  spacing={1} display='flex' justifyContent='flex-end'>
+        {links.map((link) => (
+          <Grid2 key={link}>
+            <BuyButton variant='body1' href={link}>
+              kaufen
+            </BuyButton>
+          </Grid2>
+        ))}
+      </Grid2>
   )
 }
 

0ebbb0641fd92f859f74b4dbdd01dab6a540212e
Maximilian Franz <max@modugen.de>
2022-08-12 17:33:22 +0200

    Add basic cookie consent and initialise Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import ReactGA from 'react-ga'
 import { Route, Routes } from 'react-router-dom'
 import { LandingPage } from './pages/LandingPage'
 import { ConfiguratorPage } from './pages/ConfiguratorPage'
@@ -8,6 +9,7 @@ import { useConfiguratorStore } from './stores/configuratorStore'
 import { ErrorBoundary } from 'react-error-boundary'
 import { Button, Card, CardActions, CardContent, Stack, Typography } from '@mui/material'
 import { grey } from '@mui/material/colors'
+import CookieConsent from 'react-cookie-consent'
 
 function App() {
   const resetConfiguratorStore = useConfiguratorStore((state) => state.clear)
@@ -48,6 +50,25 @@ function App() {
           <Route path='configurator/*' element={<ConfiguratorPage />} />
           <Route path='results' element={<ResultPage />} />
         </Routes>
+        <CookieConsent
+          location='bottom'
+          buttonText='Alle Akzeptieren'
+          style={{ background: '#3D802D' }}
+          buttonStyle={{
+            color: '#4e503b',
+            fontSize: '13px',
+            fontFamily: 'arial',
+            backgroundColor: 'white',
+          }}
+          expires={150}
+          onAccept={() => {
+            ReactGA.initialize('G-HBVRKMT6YZ')
+            ReactGA.pageview(window.location.pathname + window.location.search)
+          }}
+        >
+          Diese Website verwendet Cookies, um die Funktionalität bereitzustellen. Leider ist eine
+          Nutzung ohne Cookies aktuell nicht möglich!
+        </CookieConsent>
       </Layout>
     </ErrorBoundary>
   )

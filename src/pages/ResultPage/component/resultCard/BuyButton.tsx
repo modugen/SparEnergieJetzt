2f0dae8ea18f550b74dea0bdf47d47621bb1f519
chris <chris@modugen.de>
2022-08-16 17:30:29 +0200

    Try to integrate google ads

--- src/pages/ResultPage/component/resultCard/BuyButton.tsx
+++ src/pages/ResultPage/component/resultCard/BuyButton.tsx
@@ -1,6 +1,7 @@
+import { Button } from '@mui/material'
 import Grid2 from '@mui/material/Unstable_Grid2'
 import React from 'react'
-import { BuyButton } from '../styled'
+// import { BuyButton } from '../styled'
 
 const BuyButtons = ({
   links,
@@ -10,13 +11,32 @@ const BuyButtons = ({
     link: string
   }[]
 }) => {
+  const gtagReportConversion = (url: (string | Location) & Location) => {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    gtag('event', 'conversion', {
+      // eslint-disable-next-line camelcase
+      send_to: 'AW-10949782711/YHtpCJ3K6tEDELfZoeUo',
+      // eslint-disable-next-line camelcase
+      event_callback: () => {
+        if (typeof url != 'undefined') {
+          window.location = url
+        }
+      },
+    })
+    return false
+  }
+
   return (
     <Grid2 container spacing={1} display='flex' justifyContent='flex-end'>
       {links.map((link) => (
         <Grid2 key={link.provider}>
-          <BuyButton variant='body1' href={link.link}>
+          <Button onClick={() => gtagReportConversion(link.link as unknown as Location)}>
+            hello
+          </Button>
+          {/* <BuyButton variant='body1' href={link.link} onClick={() => gtagReportConversion(link.link as unknown as Location)}>
             {link.provider}
-          </BuyButton>
+          </BuyButton> */}
         </Grid2>
       ))}
     </Grid2>

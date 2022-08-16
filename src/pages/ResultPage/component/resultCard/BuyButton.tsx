158c89a6baf8cbcd92c7592961396f3588ecd878
chris <chris@modugen.de>
2022-08-16 18:04:29 +0200

    Add gogole ads basic integration for conversion

--- src/pages/ResultPage/component/resultCard/BuyButton.tsx
+++ src/pages/ResultPage/component/resultCard/BuyButton.tsx
@@ -1,7 +1,6 @@
-import { Button } from '@mui/material'
 import Grid2 from '@mui/material/Unstable_Grid2'
 import React from 'react'
-// import { BuyButton } from '../styled'
+import { BuyButton } from '../styled'
 
 const BuyButtons = ({
   links,
@@ -31,12 +30,9 @@ const BuyButtons = ({
     <Grid2 container spacing={1} display='flex' justifyContent='flex-end'>
       {links.map((link) => (
         <Grid2 key={link.provider}>
-          <Button onClick={() => gtagReportConversion(link.link as unknown as Location)}>
-            hello
-          </Button>
-          {/* <BuyButton variant='body1' href={link.link} onClick={() => gtagReportConversion(link.link as unknown as Location)}>
+          <BuyButton variant='body1' onClick={() => gtagReportConversion(link.link as unknown as Location)}>
             {link.provider}
-          </BuyButton> */}
+          </BuyButton>
         </Grid2>
       ))}
     </Grid2>

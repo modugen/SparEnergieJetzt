50f7a01bd60daef16016b25063a165df21c7cbd0
chris <chris@modugen.de>
2022-08-22 15:30:31 +0200

    Correctly implement google analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,26 @@
 import './App.css'
-// import ReactGA from 'react-ga'
 import { Route, Routes } from 'react-router-dom'
 import { LandingPage } from './pages/LandingPage'
 import { ConfiguratorPage } from './pages/ConfiguratorPage'
 import { ResultPage } from './pages/ResultPage'
 import Layout from './components/layout'
-import CookieConsent from // getCookieConsentValue
+import CookieConsent, {getCookieConsentValue} from // getCookieConsentValue
 'react-cookie-consent'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 function App() {
   const isReactSnap = navigator.userAgent === 'ReactSnap'
-
-  useEffect(() => {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    // eslint-disable-next-line camelcase
-    // gtag('event', 'conversion', { send_to: 'AW-10949782711/c1xwCIanuNEDELfZoeUo' })
-  }, [])
-
-  const initializeGa = () => {
-    // const cookieVal = getCookieConsentValue()
-    // if (cookieVal === 'true') {
-    //   ReactGA.initialize('G-HBVRKMT6YZ', {
-    //     gaOptions: {
-    //       siteSpeedSampleRate: 100,
-    //     },
-    //   })
-    //   ReactGA.pageview(window.location.pathname + window.location.search)
-    //   console.log('Google analytics initialized')
-    // } else {
-    //   console.log('Google analytics could not be initialized as cookies are not allowed')
-    //   console.log(cookieVal)
-    // }
+  const [, setCookiesConsented] = useState(false)
+  const getAndSetCookiesConstented = () => {
+    const cookieVal = getCookieConsentValue()
+    if (cookieVal === 'true'){
+      console.log('Cookies have been enabled')
+      setCookiesConsented(true)
+    } else setCookiesConsented(false)
   }
 
   useEffect(() => {
-    initializeGa()
+    getAndSetCookiesConstented()
   }, [])
 
   return (
@@ -58,7 +42,7 @@ function App() {
             backgroundColor: 'white',
           }}
           expires={150}
-          onAccept={initializeGa}
+          onAccept={getAndSetCookiesConstented}
         >
           Diese Website verwendet Cookies, um die Funktionalität bereitzustellen. Leider ist eine
           Nutzung ohne Cookies aktuell nicht möglich!
